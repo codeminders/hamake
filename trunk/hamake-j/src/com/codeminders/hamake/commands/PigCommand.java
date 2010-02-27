8c40aeb4422c7c1d8be1150f63bb30e02b2856fd
lyolik1977 <lyolik1977@b2eb0666-27cf-11de-a2f3-1373824ab735>
2010-02-27 00:43:13 +0000

    Executing PIG commands using API calls

--- trunk/hamake-j/src/com/codeminders/hamake/commands/PigCommand.java
+++ trunk/hamake-j/src/com/codeminders/hamake/commands/PigCommand.java
@@ -1,35 +1,31 @@
 package com.codeminders.hamake.commands;
 
+import com.codeminders.hamake.Config;
 import com.codeminders.hamake.Param;
 import com.codeminders.hamake.Utils;
-import com.codeminders.hamake.Config;
 import com.codeminders.hamake.params.PigParam;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.hadoop.hdfs.DFSClient;
+import org.apache.pig.ExecType;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.impl.PigContext;
+import org.apache.pig.tools.grunt.Grunt;
+import org.apache.pig.tools.parameters.ParameterSubstitutionPreprocessor;
+import org.apache.pig.tools.parameters.ParseException;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
-import java.io.IOException;
+import java.util.Properties;
 
 public class PigCommand extends BaseCommand {
-
-    /**
-     * Default name of pig executable
-     */
-    public static final String PIGCMD = "pig";
-
-    /**
-     * Name of env. var. which holds name of pig executable
-     */
-    public static final String PIGCMDENV = "PIG";
 
     private String script;
 
     public PigCommand() {
     }
-    
+
     public PigCommand(String script, Collection<Param> parameters) {
         setScript(script);
         setParameters(parameters);
@@ -38,7 +34,6 @@ public class PigCommand extends BaseCommand {
     public int execute(Map<String, Collection> parameters, Map<String, Object> context) {
         DFSClient fsClient = Utils.getFSClient(context);
         Collection<String> args = new ArrayList<String>();
-        args.add(Utils.getenv(PIGCMDENV, PIGCMD));
 
         Collection<Param> scriptParams = getParameters();
         if (scriptParams != null) {
@@ -50,7 +45,7 @@ public class PigCommand extends BaseCommand {
                         values = p.get(parameters, fsClient);
                     } catch (IOException ex) {
                         System.err.println("Failed to extract parameter values from parameter " +
-                                pp.getName() + ": " +  ex.getMessage());
+                                pp.getName() + ": " + ex.getMessage());
                         if (Config.getInstance().test_mode)
                             ex.printStackTrace();
                         return -1000;
@@ -59,14 +54,60 @@ public class PigCommand extends BaseCommand {
                         System.err.println("Multiple values for param are no supported in PIG scripts");
                         return -1000;
                     }
-                    args.add("-param");
                     args.add(pp.getName() + '=' + values.iterator().next());
                 }
             }
         }
-        args.add("-f");
-        args.add(getScript());
-        return Utils.execute(StringUtils.join(args, ' '));
+
+        // TODO: Do we need to define some properties?
+        Properties pigProps = new Properties();
+        PigContext ctx = new PigContext(ExecType.MAPREDUCE, pigProps);
+
+        try {
+            // Run, using the provided file as a pig file
+            BufferedReader in = new BufferedReader(new FileReader(getScript()));
+            // run parameter substitution preprocessor first
+            File substFile = File.createTempFile("subst", ".pig");
+            BufferedReader pin = preprocessPigScript(in, args, substFile, Config.getInstance().dryrun);
+            if (Config.getInstance().dryrun) {
+                System.err.println("Substituted pig script is at " + substFile);
+                return 0;
+            }
+
+            // Set job name based on name of the script
+            ctx.getProperties().setProperty(PigContext.JOB_NAME,
+                                                   "PigLatin:" + getScript());
+
+            substFile.deleteOnExit();
+
+            Grunt grunt = new Grunt(pin, ctx);
+            int results[] = grunt.exec();
+            // results:
+            // 0: succeeded
+            // 1: failed
+            return results[1] == 0 ? 0 : -1000;
+
+        } catch (ExecException ex) {
+            System.err.println("Failed to execute PIG command " + getScript() + ": " + ex.getMessage());
+            if (Config.getInstance().test_mode)
+                ex.printStackTrace();
+            return -1000;
+        } catch (IOException ex) {
+            System.err.println("Failed to execute PIG command " + getScript() + ": " + ex.getMessage());
+            if (Config.getInstance().test_mode)
+                ex.printStackTrace();
+            return -1000;
+        } catch (ParseException ex) {
+            System.err.println("Failed to execute PIG command " + getScript() + ": " + ex.getMessage());
+            if (Config.getInstance().test_mode)
+                ex.printStackTrace();
+            return -1000;
+        } catch (Throwable ex) {
+            System.err.println("Failed to execute PIG command " + getScript() + ": " + ex.getMessage());
+            if (Config.getInstance().test_mode)
+                ex.printStackTrace();
+            return -1000;
+        }
     }
 
     public String getScript() {
@@ -78,8 +119,30 @@ public class PigCommand extends BaseCommand {
     }
 
     @Override
-     public String toString() {
-         return new ToStringBuilder(this).append("script", script).appendSuper(super.toString()).toString();
-     }
-    
+    public String toString() {
+        return new ToStringBuilder(this).append("script", script).appendSuper(super.toString()).toString();
+    }
+
+    protected BufferedReader preprocessPigScript(BufferedReader origPigScript,
+                                                 Collection<String> params,
+                                                 File scriptFile,
+                                                 boolean createFile)
+            throws ParseException, IOException {
+        ParameterSubstitutionPreprocessor psp = new ParameterSubstitutionPreprocessor(50);
+        String[] type1 = new String[1];
+
+        if (createFile) {
+            BufferedWriter fw = new BufferedWriter(new FileWriter(scriptFile));
+            psp.genSubstitutedFile(origPigScript, fw, params.size() > 0 ? params.toArray(type1) : null,
+                    null);
+            return new BufferedReader(new FileReader(scriptFile));
+
+        } else {
+            StringWriter writer = new StringWriter();
+            psp.genSubstitutedFile(origPigScript, writer, params.size() > 0 ? params.toArray(type1) : null,
+                    null);
+            return new BufferedReader(new StringReader(writer.toString()));
+        }
+    }
+
 }
