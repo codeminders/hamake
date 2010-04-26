d325d36c91a2933f6cb37a3217a988c6a981d5bb
vorl.soft <vorl.soft@b2eb0666-27cf-11de-a2f3-1373824ab735>
2010-04-26 17:30:43 +0000

    errors in XSD file have been corrected

--- trunk/hamake-j/src/java/com/codeminders/hamake/data/SetDataFunction.java
+++ trunk/hamake-j/src/java/com/codeminders/hamake/data/SetDataFunction.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
