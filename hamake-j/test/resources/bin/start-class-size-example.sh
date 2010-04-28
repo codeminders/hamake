17d5372af43e5780afc6e9c2b6a298a6748907d3
abondar <abondar@b2eb0666-27cf-11de-a2f3-1373824ab735>
2010-04-28 13:32:11 +0000

    shell script for examples execution

---
+++ hamake-j/test/resources/bin/start-class-size-example.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+hadoop fs -rmr /user/$USERNAME/build
+hadoop fs -rmr /user/$USERNAME/dist
+hadoop fs -rmr /user/$USERNAME/test
+
+hadoop fs -mkdir dist/examples
+hadoop fs -mkdir build/lib
+hadoop fs -mkdir test/resources/scripts
+
+hadoop fs -put ../*.jar dist/examples
+hadoop fs -put /usr/lib/hamake/*.jar build/lib
+hadoop fs -put /usr/lib/hamake/*.jar dist
+hadoop fs -put ../scripts/*.pig test/resources/scripts
+
+hadoop jar /usr/lib/hamake/hamake-1.0.jar -f file:///usr/local/hamake/hamakefiles/class-size.xml
