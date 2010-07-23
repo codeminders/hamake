68459563719588b0d1bf32a1f20118b1fadd0854
vorl.soft <vorl.soft@b2eb0666-27cf-11de-a2f3-1373824ab735>
2010-07-23 12:53:18 +0000

    fixes issue #39

--- src/java/com/codeminders/hamake/ExecutionGraph.java
+++ src/java/com/codeminders/hamake/ExecutionGraph.java
@@ -5,7 +5,8 @@ import java.util.List;
 interface ExecutionGraph {
 
     List<String> getReadyForRunTasks();
-    public List<String> getReadyForRunTasks(String[] targets); 
+    public List<String> getReadyForRunTasks(String[] targets);
+    public List<String> getDependentTasks(String task);
     void removeTask(String name);
     
 }
